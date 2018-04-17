173b8ccc91ebbdac6a5dc73553067369faadc209
Kája <kaja@joja.cz>
2018-04-17 07:45:24 +0200

    změněn název proměnne

--- projekty 4/xctverecek.py
+++ projekty 4/xctverecek.py
@@ -1,7 +1,7 @@
 v=10
 for i in range(v):
-	for l in range(v):
-		if i >0 and i < v-1 and l > 0 and l < v-1 :
+	for k in range(v):
+		if i >0 and i < v-1 and k > 0 and k < v-1 :
 			print(" ", end = " ")
 		else:
 			print( "X" , end = " ")
